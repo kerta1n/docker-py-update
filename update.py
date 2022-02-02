63e28a0e52461480ea3f7d7ac1711ebe06a05fba
kerta1n <36344851+kerta1n@users.noreply.github.com>
2022-02-02 15:30:50 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,14 +1,15 @@
 import os
 import sys
 def update():
+  print("Welcome to the Docker update script! This script updates system packages in order to ensure proper container updates. Press space to update system packages. Press control and c to stop.")
+  i = input()
   print("Updating host OS packages, please enter sudo password if needed...")
   os.system("sudo apt update")
   os.system("sudo apt upgrade")
-  print("Welcome to the Docker update script! Type the directory name of the container that you want to update, exactly as it shows. (Run ls in this directory if you're not sure and hit control and c at any time to stop this script.)")
+  print("Purging and autoremoving unused packages to free up disk space...")
+  os.system("sudo apt remove --purge --autoremove") 
+  print("Type the directory name of the container that you want to update, exactly as it shows. (Run ls in this directory if you're not sure and hit control and c at any time to stop this script.)")
   target = input()
-# if target in ["all"]:
-# else: 
-#   pass
   os.system("docker compose -f " + target + "/docker-compose.yml up -d --force-recreate")
   print("Would you like to exit, or restart the script? Type your choice exactly as written. ")
   done = input()
